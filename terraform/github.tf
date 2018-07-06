2c430d71fc8a02e15a0066cd548e3fdd650dc87b
Michael Aldridge <aldridge.mac@gmail.com>
2018-07-06 15:55:59 -0700

    Terraform GitHub

---
+++ terraform/github.tf
@@ -0,0 +1,255 @@
+provider "github" {
+  organization = "void-linux"
+}
+
+#####################
+# Core Repositories #
+#####################
+resource "github_repository" "void-infrastructure" {
+  name        = "void-infrastructure"
+  description = "Infrastructure configuration data for Void systems"
+  has_issues = true
+  homepage_url = "https://voidlinux.org"
+  allow_merge_commit = false
+}
+
+resource "github_repository" "void-packages" {
+  name        = "void-packages"
+  description = "The Void source packages collection"
+  has_issues = true
+  homepage_url = "https://voidlinux.org"
+}
+
+resource "github_repository" "void-mklive" {
+  name = "void-mklive"
+  description = "The Void Linux live image maker"
+  has_issues = true
+  homepage_url = "https://voidlinux.org"
+}
+
+resource "github_repository" "void-wiki" {
+  name = "void-wiki"
+  description = "Components for wiki.voidlinux.org"
+  has_issues = true
+  homepage_url = "https://voidlinux.org"
+}
+
+resource "github_repository" "void-texlive" {
+  name = "void-texlive"
+  description = "Components for packaging TeX into XBPS"
+  has_issues = true
+  homepage_url = "https://voidlinux.org"
+}
+
+resource "github_repository" "void-runit" {
+  name = "void-runit"
+  description = "runit init scripts for Void"
+  has_issues = true
+  homepage_url = "https://voidlinux.org"
+}
+
+resource "github_repository" "void-updates" {
+  name = "void-updates"
+  description = "Update check system for void-packages"
+  has_issues = true
+  homepage_url = "https://voidlinux.org"
+}
+
+resource "github_repository" "void-linux-website" {
+  name = "void-linux.github.io"
+  description = "Void Linux website"
+  has_issues = true
+  homepage_url = "https://voidlinux.org"
+}
+
+resource "github_repository" "socklog-void" {
+  name = "socklog-void"
+  description = "SockLog configuration for Void Linux"
+  has_issues = true
+  homepage_url = "https://voidlinux.org"
+}
+
+resource "github_repository" "xbps" {
+  name = "xbps"
+  description = "The X Binary Package System"
+  has_issues = true
+  homepage_url = "https://voidlinux.org/xbps/"
+}
+
+resource "github_repository" "xbps-bulk" {
+  name = "xbps-bulk"
+  description = "Builds a set of packages that need to be updated with inter-dependencies."
+  has_issues = true
+  homepage_url = "https://voidlinux.org"
+}
+
+###########################
+# Auxilliary Repositories #
+###########################
+
+resource "github_repository" "netbsd-wtf" {
+  name = "netbsd-wtf"
+  description = "NetBSD's wtf(6) adapted for Void Linux"
+  has_issues = true
+}
+
+resource "github_repository" "libglob" {
+  name = "libglob"
+  description = "BSD glob(3) implementation with non-POSIX features"
+  has_issues = true
+}
+
+resource "github_repository" "jbigkit-shared" {
+  name = "jbigkit-shared"
+  description = "Autotools and libtool for JBIG-KIT version 2.1"
+  has_issues = true
+}
+
+resource "github_repository" "musl-obstack" {
+  name = "musl-obstack"
+  description = "A standalone library to implement GNU libc's obstack"
+  has_issues = true
+}
+
+resource "github_repository" "musl-fts" {
+  name = "musl-fts"
+  description = "Implementation of fts(3) for musl libc packages in Void Linux"
+  has_issues = true
+}
+
+resource "github_repository" "openbsd-man" {
+  name = "openbsd-man"
+  description = "The OpenBSD man(1) utility for Linux"
+  has_issues = true
+}
+
+#########
+# Teams #
+#########
+
+resource "github_team" "pkg-committers" {
+  name = "pkg-committers"
+  description = "The people with push access"
+  privacy = "closed"
+}
+
+resource "github_team" "void-ops" {
+  name = "void-ops"
+  description = "Infrastructure Operators"
+  privacy = "closed"
+}
+
+###############
+# Memberships #
+###############
+
+# See github_members.tf.
+
+##########################
+# Membership Permissions #
+##########################
+
+resource "github_team_repository" "repo-void-infrastructure" {
+  # The infrastructure repo is special and has restricted push so that
+  # its harder to accidentally break systems that are subscribed to
+  # it.
+  team_id    = "${github_team.void-ops.id}"
+  repository = "${github_repository.void-infrastructure.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "void-packages" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.void-packages.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "void-mklive" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.void-mklive.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "void-wiki" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.void-wiki.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "void-texlive" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.void-texlive.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "void-runit" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.void-runit.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "void-updates" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.void-updates.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "void-linux-website" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.void-linux-website.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "socklog-void" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.socklog-void.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "xbps" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.xbps.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "xbps-bulk" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.xbps-bulk.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "netbsd-wtf" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.netbsd-wtf.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "libglob" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.libglob.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "jbigkit-shared" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.jbigkit-shared.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "musl-obstack" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.musl-obstack.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "musl-fts" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.musl-fts.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "openbsd-man" {
+  team_id    = "${github_team.pkg-committers.id}"
+  repository = "${github_repository.openbsd-man.name}"
+  permission = "push"
+}
---
+++ terraform/github_members.tf
@@ -0,0 +1,146 @@
+######################
+# GitHub Memberships #
+######################
+
+# You might expect that this file would contain a bunch of loops that
+# compute what memberships people are supposed to have and then add
+# them to it.  No, that would require Terraform to support sane state
+# transitions which it doesn't.  In order to add or remove people in
+# github, terraform nukes the membership and then adds everyone back
+# as requested.
+
+# Yes, this includes the organization owners.
+
+# Literally the only thing that saved us while running this in setup
+# was that GitHub's API won't let you delete yourself from the
+# organization if you're the last man standing.  So now we have this
+# expanded monstrosity which puts people in groups the long way round.
+
+#######################
+# Organization Owners #
+#######################
+
+resource "github_membership" "org-owner_gottox" {
+  username = "Gottox"
+  role = "admin"
+}
+
+resource "github_membership" "org-owner_duncaen" {
+  username = "Duncaen"
+  role = "admin"
+}
+
+resource "github_membership" "org-owner_the-maldridge" {
+  username = "the-maldridge"
+  role = "admin"
+}
+
+###################
+# Void Operations #
+###################
+
+# These memberships belong to groups defined in github.tf.  These are
+# sorted by lexical sort order keyed on the username.  This order is
+# manually preserved, please be careful when editing.
+
+resource "github_team_membership" "void-ops_duncaen" {
+  team_id = "${github_team.void-ops.id}"
+  role = "maintainer"
+  username = "Duncaen"
+}
+
+resource "github_team_membership" "void-ops_gottox" {
+  team_id = "${github_team.void-ops.id}"
+  role = "maintainer"
+  username = "Gottox"
+}
+
+resource "github_team_membership" "void-ops_vaelatern" {
+  team_id = "${github_team.void-ops.id}"
+  role = "maintainer"
+  username = "Vaelatern"
+}
+
+resource "github_team_membership" "void-ops_the-maldridge" {
+  team_id = "${github_team.void-ops.id}"
+  role = "maintainer"
+  username = "the-maldridge"
+}
+
+######################
+# Package Committers #
+######################
+
+# These memberships belong to groups defined in github.tf.  These are
+# sorted by lexical sort order keyed on the username.  This order is
+# manually preserved, please be careful when editing.
+
+# Maintainers
+
+resource "github_team_membership" "pkg-committers_duncaen" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "maintainer"
+  username = "Duncaen"
+}
+
+resource "github_team_membership" "pkg-committers_gottox" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "maintainer"
+  username = "Gottox"
+}
+
+resource "github_team_membership" "pkg-committers_the-maldridge" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "maintainer"
+  username = "the-maldridge"
+}
+
+# Members
+
+resource "github_team_membership" "pkg-committers_vaelatern" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "member"
+  username = "Vaelatern"
+}
+
+resource "github_team_membership" "pkg-committers_asergi" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "member"
+  username = "asergi"
+}
+
+resource "github_team_membership" "pkg-committers_chneukirchen" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "member"
+  username = "chneukirchen"
+}
+
+resource "github_team_membership" "pkg-committers_cr6git" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "member"
+  username = "cr6git"
+}
+
+resource "github_team_membership" "pkg-committers_lemmi" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "member"
+  username = "lemmi"
+}
+
+resource "github_team_membership" "pkg-committers_maxice8" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "member"
+  username = "maxice8"
+}
+
+resource "github_team_membership" "pkg-committers_pullmoll" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "member"
+  username = "pullmoll"
+}
+
+resource "github_team_membership" "pkg-committers_thypon" {
+  team_id = "${github_team.pkg-committers.id}"
+  role = "member"
+  username = "thypon"
+}
