6e872af1444be7bf93babe48bf93edebc9844b11
Michael Aldridge <aldridge.mac@gmail.com>
2020-12-13 14:34:23 -0800

    terraform/do: List generated_content bucket as a production resource

--- terraform/do/project.tf
+++ terraform/do/project.tf
@@ -9,6 +9,7 @@ resource "digitalocean_project_resources" "void" {
   project = digitalocean_project.void_prod.id
   resources = [
     digitalocean_spaces_bucket.custom_images.urn,
+    digitalocean_spaces_bucket.generated_content.urn,
     digitalocean_droplet.a_sfo3_us.urn,
     digitalocean_droplet.b_sfo3_us.urn,
     digitalocean_droplet.c_sfo3_us.urn,
