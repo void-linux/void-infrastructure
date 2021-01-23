2114a1a247338b86442ea9c0d80fca770aabaa07
Michael Aldridge <aldridge.mac@gmail.com>
2021-01-23 00:19:29 -0800

    Add minio policy for nbuild

--- terraform/hashistack/nomad_namespaces.tf
+++ terraform/hashistack/nomad_namespaces.tf
@@ -14,6 +14,6 @@ resource "nomad_namespace" "apps" {
 }
 
 resource "nomad_namespace" "build" {
-  name = "build"
+  name        = "build"
   description = "Home of build related tasks"
 }
--- terraform/hashistack/policy_minio.tf
+++ terraform/hashistack/policy_minio.tf
@@ -1,13 +1,25 @@
 data "vault_policy_document" "secrets_minio" {
   rule {
-    path = "secret/minio/*"
+    path         = "secret/minio/*"
     capabilities = ["read"]
-    description = "Read minio secrets"
+    description  = "Read minio secrets"
   }
 }
 
 resource "vault_policy" "secrets_minio" {
-  name = "void-secrets-minio"
+  name   = "void-secrets-minio"
   policy = data.vault_policy_document.secrets_minio.hcl
 }
 
+data "vault_policy_document" "secrets_minio_nbuild" {
+  rule {
+    path         = "secret/minio/nbuild"
+    capabilities = ["read"]
+    description  = "Read nbuild credentials"
+  }
+}
+
+resource "vault_policy" "secrets_minio_nbuild" {
+  name   = "void-secrets-minio-nbuild"
+  policy = data.vault_policy_document.secrets_minio_nbuild.hcl
+}
