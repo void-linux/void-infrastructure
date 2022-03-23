f9ef7ac41b9bcd376dbb932362524efa80f064f3
Michael Aldridge <aldridge.mac@gmail.com>
2022-03-22 20:28:21 -0500

    terraform/do: upscale control hosts

--- terraform/do/controller.tf
+++ terraform/do/controller.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "b_sfo3_us" {
   image  = data.digitalocean_image.void_20200730RC1.id
   name   = "b-sfo3-us"
   region = "sfo3"
-  size   = "s-1vcpu-1gb"
+  size   = "s-1vcpu-2gb"
 
   vpc_uuid = digitalocean_vpc.main.id
   ssh_keys = [digitalocean_ssh_key.maldridge1.fingerprint]
@@ -12,7 +12,7 @@ resource "digitalocean_droplet" "c_sfo3_us" {
   image  = data.digitalocean_image.void_20200730RC1.id
   name   = "c-sfo3-us"
   region = "sfo3"
-  size   = "s-1vcpu-1gb"
+  size   = "s-1vcpu-2gb"
 
   vpc_uuid = digitalocean_vpc.main.id
   ssh_keys = [digitalocean_ssh_key.maldridge1.fingerprint]
@@ -22,7 +22,7 @@ resource "digitalocean_droplet" "d_sfo3_us" {
   image  = data.digitalocean_image.void_20200730RC1.id
   name   = "d-sfo3-us"
   region = "sfo3"
-  size   = "s-1vcpu-1gb"
+  size   = "s-1vcpu-2gb"
 
   vpc_uuid = digitalocean_vpc.main.id
   ssh_keys = [digitalocean_ssh_key.maldridge1.fingerprint]
