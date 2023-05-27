9e2e86bfd20428b773bdbe32b85b5f0e60c152ff
Michael Aldridge <aldridge.mac@gmail.com>
2023-05-27 18:14:08 -0500

    terraform/hashistack: Add void-secrets-tls policy

--- terraform/hashistack/policy_tls.tf
+++ terraform/hashistack/policy_tls.tf
@@ -20,3 +20,18 @@ resource "vault_policy" "secrets_traefik" {
   name   = "void-secrets-traefik"
   policy = data.vault_policy_document.secrets_traefik.hcl
 }
+
+data "vault_policy_document" "secrets_tls_certs" {
+  rule {
+    path = "secret/lego/data/certificates/*"
+    capabilities = [
+      "read"
+    ]
+    description = "Access to TLS Certificates"
+  }
+}
+
+resource "vault_policy" "secrets_tls_certs" {
+  name   = "void-secrets-tls"
+  policy = data.vault_policy_document.secrets_tls_certs.hcl
+}
