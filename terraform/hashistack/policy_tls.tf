26bb4b5378ecfdaadf48eb9048741fb77b5aec65
Michael Aldridge <aldridge.mac@gmail.com>
2023-05-27 18:16:12 -0500

    terraform/hashistack: Remove obsolete traefik policy

--- terraform/hashistack/policy_tls.tf
+++ terraform/hashistack/policy_tls.tf
@@ -1,26 +1,3 @@
-data "vault_policy_document" "secrets_traefik" {
-  rule {
-    path = "secret/traefik/*"
-    capabilities = [
-      "read",
-    ]
-    description = "Read Traefik Secrets"
-  }
-
-  rule {
-    path = "secret/lego/data/certificates/*"
-    capabilities = [
-      "read"
-    ]
-    description = "Traefik uses LEGO certificates"
-  }
-}
-
-resource "vault_policy" "secrets_traefik" {
-  name   = "void-secrets-traefik"
-  policy = data.vault_policy_document.secrets_traefik.hcl
-}
-
 data "vault_policy_document" "secrets_tls_certs" {
   rule {
     path = "secret/lego/data/certificates/*"
