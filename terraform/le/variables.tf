0b77a6266ea96ae5ebab6756a5dbc327501cda36
Michael Aldridge <aldridge.mac@gmail.com>
2025-04-12 17:56:03 -0500

    terraform/le: Refactor variables interactions

--- terraform/le/variables.tf
+++ terraform/le/variables.tf
@@ -1,16 +1,12 @@
-resource "nomad_variable" "omniproxy" {
-  namespace = "mirror"
-  path = "nomad/jobs/nginx"
-
-  items = {
-    certificate = "${acme_certificate.voidlinux_org.certificate_pem}${acme_certificate.voidlinux_org.issuer_pem}"
-    key = acme_certificate.voidlinux_org.private_key_pem
+resource "nomad_variable" "certs" {
+  for_each = {
+    "nomad/jobs/nginx" = "mirror"
+    "nomad/jobs/nginx-control" = "infrastructure"
+    "nomad/jobs/maddy" = "apps-restricted"
   }
-}
 
-resource "nomad_variable" "controlproxy" {
-  namespace = "infrastructure"
-  path = "nomad/jobs/nginx-control"
+  namespace = each.value
+  path = each.key
 
   items = {
     certificate = "${acme_certificate.voidlinux_org.certificate_pem}${acme_certificate.voidlinux_org.issuer_pem}"
