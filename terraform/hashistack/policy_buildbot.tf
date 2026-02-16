301aae86217b9c24fed83ac28f5da3d06d30e47e
classabbyamp <void@placeviolette.net>
2026-02-16 01:05:26 -0500

    terraform/**: terraform fmt

--- terraform/hashistack/policy_buildbot.tf
+++ terraform/hashistack/policy_buildbot.tf
@@ -1,10 +1,10 @@
 resource "nomad_acl_policy" "buildbot_worker_admin" {
-  name = "buildbot-worker-admin"
+  name        = "buildbot-worker-admin"
   description = "Manage buildbot worker secrets in nomad variables"
 
   job_acl {
     namespace = "build"
-    job_id = "buildbot"
+    job_id    = "buildbot"
   }
 
   rules_hcl = <<EOT
--- terraform/hashistack/policy_popcorn.tf
+++ terraform/hashistack/policy_popcorn.tf
@@ -1,10 +1,10 @@
 resource "nomad_acl_policy" "popcorn_admin" {
-  name = "popcorn-admin"
+  name        = "popcorn-admin"
   description = "Manage popcorn keys in nomad variables"
 
   job_acl {
     namespace = "apps"
-    job_id = "popcorn-report"
+    job_id    = "popcorn-report"
   }
 
   rules_hcl = <<EOT
